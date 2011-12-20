0917253893f6843fde994a4fd57c5080a7fb9366
Fabian Kajzar <fabiankajzar@googlemail.com>
2011-12-20 18:21:03 +0100

    Upload Widget: - wrong import removed

--- src/de/beepublished/client/widget/UploadSourceWidget.java
+++ src/de/beepublished/client/widget/UploadSourceWidget.java
@@ -10,8 +10,6 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.wb.swt.layout.grouplayout.GroupLayout;
-import org.eclipse.wb.swt.layout.grouplayout.LayoutStyle;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
 
