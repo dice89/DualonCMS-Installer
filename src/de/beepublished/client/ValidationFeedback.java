ed9fa040f2abd9b7374e60ae47e8c83772c04874
Fabian Kajzar <fabiankajzar@googlemail.com>
2012-02-16 23:26:23 +0100

    unused files removed

--- src/de/beepublished/client/ValidationFeedback.java
+++
@@ -1,7 +0,0 @@
-package de.beepublished.client;
-
-public interface ValidationFeedback {
-	public void setValidationFeedback(int status);
-	public void resolveSource();
-	public void resolveTarget();
-}
--- src/de/beepublished/client/ValidationStatus.java
+++
@@ -1,8 +0,0 @@
-package de.beepublished.client;
-
-public class ValidationStatus {
-	public static final int VALID_INSTALL = 1;
-	public static final int VALID_BACKUP = 2;
-	public static final int INVALID = 0;
-	public static final int VALID_MIGRATION = 3;
-}
